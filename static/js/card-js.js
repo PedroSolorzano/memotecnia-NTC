8b01e7898751cac6f31c2fa4a0aa06f9a0c6bd06
Keivou <keivou@desktop.local>
2023-02-27 11:11:11 -0500

    change card flip to 3 seconds

--- static/js/card-js.js
+++ static/js/card-js.js
@@ -1,4 +1,4 @@
 setTimeout(function() {
   const card = document.querySelector('.card');
   card.classList.add('flipped');
-}, 500); // Flip the card after 5 seconds (5000 milliseconds)
+}, 3000); // Flip the card after 5 seconds (5000 milliseconds)
