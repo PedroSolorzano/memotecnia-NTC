c095b3ecd7d821a93337508d6f1734d7439d1e45
Keivou <keivou@desktop.local>
2023-03-02 10:38:15 -0500

    update to 15 seconds

--- static/js/card-js.js
+++ static/js/card-js.js
@@ -1,4 +1,4 @@
 setTimeout(function() {
   const card = document.querySelector('.card');
   card.classList.add('flipped');
-}, 3000); // Flip the card after 5 seconds (5000 milliseconds)
+}, 15000); // Flip the card after 5 seconds (5000 milliseconds)
